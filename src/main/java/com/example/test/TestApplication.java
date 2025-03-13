c9bac929a23d92e0d63080846352d7a4b13510da
Asish <asishkumarroutray1789@gmail.com>
2025-03-13 16:25:56 +0530

    Your commit message

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -8,11 +8,11 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		int x = 100;
-		int y 200;
+		int y= 200;
 		int z = 500;
-		system.out.println(x);
-		system.out.println(y);
-		system.out.println(z);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
 		SpringApplication.run(TestApplication.class, args);
 	}
 
