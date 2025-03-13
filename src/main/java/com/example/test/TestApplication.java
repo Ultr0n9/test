0dcf3518d58903aeb4957372904e85bf543b74f1
Asish Kumar Routray <167056703+Ultr0n9@users.noreply.github.com>
2025-03-13 16:19:40 +0530

    Update TestApplication.java
    Second Commit

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		int x = 100;
+		int y 200;
+		int z = 500;
+		system.out.println(x);
+		system.out.println(y);
+		system.out.println(z);
 		SpringApplication.run(TestApplication.class, args);
 	}
 
